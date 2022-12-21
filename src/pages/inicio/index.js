b42cf227dd7b5b492bf30fac14c4f27a44be4151
tamaracosta <tamarasantosc@gmail.com>
2022-12-21 20:21:24 +0100

    Componente Titulo

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,12 +1,23 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
+import videos from "json/db.json";
+import styles from "./Inicio.module.css";
+import Card from "components/Card";
 
 function Inicio(){
   return (
     <>
       <Cabecalho />
       <Banner imagem="home" />
+      <Titulo> <h1>Um lugar para guardar seus vídeos e filmes</h1></Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />
+
+        })}
+      </section>      
       <Rodape />
     </>
     
