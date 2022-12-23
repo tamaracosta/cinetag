6a2552610d6807159e150eb7f343fab233b0ac97
tamaracosta <tamarasantosc@gmail.com>
2022-12-23 15:02:52 +0100

    componente Favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,15 +1,24 @@
-import Banner from "components/Banner";
-import Card from "components/Card";
-import Titulo from "components/Titulo";
-import styles from "./Favoritos.module.css";
+import Banner from 'components/Banner';
+import Card from 'components/Card';
+import Titulo from 'components/Titulo';
+import { useFavoritoContext } from 'contextos/Favoritos';
+import styles from './Favoritos.module.css';
 
-function Favoritos(){
-  return (
-    <>
-    <Banner imagem="favoritos" />
-    <Titulo> <h1>Favoritos</h1></Titulo>    
-    </>
-  )
+function Favoritos() {
+    const { favorito } = useFavoritoContext();
+    return (
+        <>
+            <Banner imagem='favoritos' />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
+            </section>
+        </>
+    )
 }
 
 export default Favoritos;
