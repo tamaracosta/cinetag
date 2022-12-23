e50787aa00d55b6eb1e8156891897cd7c790e7d5
tamaracosta <tamarasantosc@gmail.com>
2022-12-23 15:10:03 +0100

    componente Pagina nao encontrada e rota

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,15 +1,18 @@
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import { useParams } from "react-router-dom";
-import styles from "./Player.module.css";
 import videos from "json/db.json";
+import NaoEncontrada from "pages/NaoEncontrada";
 
 function Player() {
   const parametro = useParams();
   const video = videos.find((video) => {
-    return video.id === Number(parametro.id);
+    return video.id === Number(parametro.id);    
   })
-  console.log(video);
+  if (!video) {
+    return <NaoEncontrada />
+  }
+  
   return (
     <>
       <Banner imagem="player" />
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import Rodape from "components/Rodape";
 import Container from "components/Container";
 import FavoritosProvider from "contextos/Favoritos";
 import Player from "pages/Player";
+import NaoEncontrada from "pages/NaoEncontrada";
 
 
 function AppRoutes(){
@@ -19,6 +20,7 @@ function AppRoutes(){
           <Route path="/" element={<Inicio />}></Route>
           <Route path="/favoritos" element={<Favoritos />}></Route>
           <Route path="/:id" element={<Player />}></Route>
+          <Route path="*" element={<NaoEncontrada />}></Route>
         </Routes>
       </FavoritosProvider>
     </Container>
