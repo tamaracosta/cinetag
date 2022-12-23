450f67c6dff63c30e535cfd9057ee5f03811e48b
tamaracosta <tamarasantosc@gmail.com>
2022-12-23 18:49:51 +0100

    deletando arquivo inutilizavel e fechando provider na rota

--- src/routes.js
+++ src/routes.js
@@ -2,29 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
-import Cabecalho from "components/Cabecalho";
-import Rodape from "components/Rodape";
-import Container from "components/Container";
-import FavoritosProvider from "contextos/Favoritos";
 import Player from "pages/Player";
 import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
 
 
 function AppRoutes(){
   return (    
-    <BrowserRouter>
-    <Cabecalho />
-    <Container>
-      <FavoritosProvider>
+    <BrowserRouter>    
         <Routes>
-          <Route path="/" element={<Inicio />}></Route>
-          <Route path="/favoritos" element={<Favoritos />}></Route>
-          <Route path="/:id" element={<Player />}></Route>
-          <Route path="*" element={<NaoEncontrada />}></Route>
-        </Routes>
-      </FavoritosProvider>
-    </Container>
-    <Rodape />
+          <Route path="/" element={<PaginaBase />}>
+            <Route index element={<Inicio />}></Route>
+            <Route path="favoritos" element={<Favoritos />}></Route>
+            <Route path=":id" element={<Player />}></Route>
+            <Route path="*" element={<NaoEncontrada />}></Route>
+          </Route>
+        </Routes>      
     </BrowserRouter>
   )
 
